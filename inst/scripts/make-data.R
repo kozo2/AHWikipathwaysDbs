8d40a54ceb69de48b96dbfde7c45a94ad79bfe33
Kozo Nishida <kozo.nishida@gmail.com>
2020-12-20 06:00:06 +0900

    Add code for BridgeDbR

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -16,3 +16,21 @@ wikipathways_species <- c("Anopheles gambiae", "Arabidopsis thaliana", "Bacillus
 for (species in wikipathways_species) {
     downloadPathwayArchive(date=wikipathways_release_date, organism=species, format="gpml")
 }
+
+# metabolite_bridge_url <- "https://ndownloader.figshare.com/files/25388453"
+# download.file(metabolite_bridge_url, "metabolites_20201104.bridge")
+
+file <- "metabolites_20201104.bridge"
+download.file(
+  "https://ndownloader.figshare.com/files/25388453",
+  location
+)
+location = normalizePath(file)
+mapper <- loadDatabase(location)
+
+# see https://bridgedb.github.io/pages/system-codes.html
+# map(mapper, "HMDB0001058", source="Ch", target="Ck")
+# map(mapper, "HMDB0001058", source="Ch", target="Ce")
+# map(mapper, "HMDB0001058", source="Ch", target="Ca")
+# map(mapper, "HMDB0001058", source="Ch", target="Dr")
+# map(mapper, "HMDB0001058", source="Ch", target="Ik")
