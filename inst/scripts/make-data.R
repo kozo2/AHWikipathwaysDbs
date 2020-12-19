06d8139215d136948ee34f1cf32a7f56f5c17744
Kozo Nishida <kozo.nishida@gmail.com>
2020-12-20 04:44:18 +0900

    Create make-data.R

---
+++ inst/scripts/make-data.R
@@ -0,0 +1,17 @@
+library(xml2)
+library(rWikiPathways)
+
+wikipathways_release_date <- "20201210"
+
+wikipathways_species <- c("Anopheles gambiae", "Arabidopsis thaliana", "Bacillus subtilis",
+    "Bos taurus", "Caenorhabditis elegans", "Canis familiaris", "Danio rerio",
+    "Drosophila melanogaster", "Equus caballus", "Escherichia coli",
+    "Gallus gallus", "Gibberella zeae", "Homo sapiens", "Hordeum vulgare",
+    "Mus musculus", "Mycobacterium tuberculosis", "Oryza sativa",
+    "Pan troglodytes", "Plasmodium falciparum", "Populus trichocarpa",
+    "Rattus norvegicus", "Saccharomyces cerevisiae", "Solanum lycopersicum",
+    "Sus scrofa", "Zea mays")
+
+for (species in wikipathways_species) {
+    downloadPathwayArchive(date=wikipathways_release_date, organism=species, format="gpml")
+}
